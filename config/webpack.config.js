331c95cf850ef46d08046f1e2b4d23a85fff912a
耀煌 <yaohuang.zhang@zkteco.com>
2019-09-08 12:41:12 +0800

    close BundleAnalyzerPlugin in production

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -521,7 +521,7 @@ module.exports = function(webpackEnv) {
       ],
     },
     plugins: [
-      new BundleAnalyzerPlugin(),
+      isEnvDevelopment && new BundleAnalyzerPlugin(),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
